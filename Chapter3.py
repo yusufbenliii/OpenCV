74bdd4e4d9acc81d63a5d67fa900ec425c2aaa75
yusufbenliii <64779678+yusufbenliii@users.noreply.github.com>
2021-02-14 00:03:34 +0300

    Update Chapter3.py

--- Chapter3.py
+++ Chapter3.py
@@ -11,7 +11,7 @@ imgResize = cv2.resize(img,(300,200))
 cv2.imshow("Image Resized", imgResize)
 print(imgResize.shape)  # 200 300 3 , width height RGB
 
-imgCropped = img[0:200,200:500]  # height 0 to 200, weight 200 to 500
+imgCropped = img[0:200,200:500]  # height 0 to 200, width 200 to 500
 cv2.imshow("Image Cropped", imgCropped)
 
 cv2.waitKey(0)
